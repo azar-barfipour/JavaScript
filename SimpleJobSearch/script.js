3be322bbd410cf37d2a9b8effc6bc0c7f60cbe03
Azar <azar.barfipour@gmail.com>
2021-12-03 16:21:03 -0800

    add pictures

--- SimpleJobSearch/script.js
+++ SimpleJobSearch/script.js
@@ -57,11 +57,12 @@ function search() {
   let x = document.getElementsByTagName("li");
   for (let j = 0; j < x.length; j++) {
     if (
-      !x[j].childNodes[0].textContent.toLocaleLowerCase().includes(input.value)
+      !x[j].childNodes[1].textContent.toLocaleLowerCase().includes(input.value)
     ) {
       x[j].style.display = "none";
     } else {
       x[j].style.display = "list-item";
+      x[j].style.display = "float";
     }
   }
 }
@@ -71,7 +72,7 @@ function filter() {
   const text = select.options[select.selectedIndex].text;
   const x = document.getElementsByTagName("li");
   for (let k = 0; k < x.length; k++) {
-    if (x[k].childNodes[1].textContent === text) {
+    if (x[k].childNodes[7].textContent === text) {
       x[k].style.display = "list-item";
     } else {
       x[k].style.display = "none";
