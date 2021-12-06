8e1be0a9e7f94ada98c915b8633b3e9ef395cec7
Azar <azar.barfipour@gmail.com>
2021-12-06 15:05:56 -0800

    work on dom

--- SimpleJobSearch/script.js
+++ SimpleJobSearch/script.js
@@ -2,53 +2,114 @@ const fetchHandler = async () => {
   const response = await fetch("./data.json");
   const data = await response.json();
   const jobCard = document.querySelector(".jobCard");
+  const divContainer = document.createElement("div");
+
+  divContainer.className = "div-container";
+  // divJob.className = "div-job";
+  // divSkill.className = "div-skill";
+  jobCard.appendChild(divContainer);
+  // divContainer.appendChild(divJob);
+  // divContainer.appendChild(divSkill);
   for (let i = 0; i < data.length; i++) {
     const Item = document.createElement("li");
+    const divJob = document.createElement("div");
+    const divSkill = document.createElement("div");
     const jobItem = document.createElement("p");
     const levelItem = document.createElement("p");
     const position = document.createElement("p");
     const logo = document.createElement("img");
-    const languages = document.createElement("p");
+    // const languages = document.createElement("p");
     const location = document.createElement("p");
-    const role = document.createElement("p");
+    // const role = document.createElement("p");
     const postedAt = document.createElement("p");
     const contract = document.createElement("p");
-    const tools = document.createElement("p");
+    // const tools = document.createElement("p");
     jobItem.textContent = data[i].company;
     levelItem.textContent = data[i].level;
     position.textContent = data[i].position;
-    languages.textContent = data[i].languages;
+    // languages.textContent = data[i].languages;
     location.textContent = data[i].location;
-    role.textContent = data[i].role;
+    // role.textContent = data[i].role;
     postedAt.textContent = data[i].postedAt;
     contract.textContent = data[i].contract;
-    tools.textContent = data[i].tools;
+    // tools.textContent = data[i].tools;
     logo.src = data[i].logo;
     Item.className = "card-item";
+    divJob.className = "div-job";
     jobItem.className = "job";
-    levelItem.className = "level";
-    levelItem.id = "level";
+    // levelItem.className = "level";
+    // levelItem.id = "level";
     position.className = "position";
     logo.className = "logo";
-    languages.className = "languages";
+    // languages.className = "languages";
     location.className = "location";
-    role.className = "role";
+    // role.className = "role";
     postedAt.className = "postedAt";
     contract.className = "contract";
-    tools.className = "tools";
-    jobCard.appendChild(Item);
-    Item.appendChild(logo);
-    Item.appendChild(jobItem);
-    Item.appendChild(position);
-    Item.appendChild(postedAt);
-    Item.appendChild(contract);
-    Item.appendChild(location);
-    Item.appendChild(role);
-    Item.appendChild(levelItem);
+    // tools.className = "tools";
+    divContainer.appendChild(Item);
+    Item.appendChild(divJob);
+    Item.appendChild(divSkill);
+    divJob.appendChild(logo);
+    divJob.appendChild(jobItem);
+    divJob.appendChild(position);
+    divJob.appendChild(postedAt);
+    divJob.appendChild(contract);
+    divJob.appendChild(location);
+    // Item.appendChild(role);
+    divSkill.appendChild(levelItem);
 
-    Item.appendChild(languages);
-    Item.appendChild(tools);
+    // Item.appendChild(languages);
+    // Item.appendChild(tools);
   }
+
+  // for (let q = 0; q < data.length; q++) {
+  //   const ItemSkill = document.createElement("li");
+  //   // const jobItem = document.createElement("p");
+  //   const levelItem = document.createElement("p");
+  //   // const position = document.createElement("p");
+  //   // const logo = document.createElement("img");
+  //   const languages = document.createElement("p");
+  //   // const location = document.createElement("p");
+  //   const role = document.createElement("p");
+  //   // const postedAt = document.createElement("p");
+  //   // const contract = document.createElement("p");
+  //   const tools = document.createElement("p");
+  //   // jobItem.textContent = data[i].company;
+  //   levelItem.textContent = data[q].level;
+  //   // position.textContent = data[i].position;
+  //   languages.textContent = data[q].languages;
+  //   // location.textContent = data[i].location;
+  //   role.textContent = data[q].role;
+  //   // postedAt.textContent = data[i].postedAt;
+  //   // contract.textContent = data[i].contract;
+  //   tools.textContent = data[q].tools;
+  //   // logo.src = data[i].logo;
+  //   ItemSkill.className = "card-item";
+  //   // jobItem.className = "job";
+  //   levelItem.className = "level";
+  //   levelItem.id = "level";
+  //   // position.className = "position";
+  //   // logo.className = "logo";
+  //   languages.className = "languages";
+  //   // location.className = "location";
+  //   role.className = "role";
+  //   // postedAt.className = "postedAt";
+  //   // contract.className = "contract";
+  //   tools.className = "tools";
+  //   divSkill.appendChild(ItemSkill);
+  //   // Item.appendChild(logo);
+  //   // Item.appendChild(jobItem);
+  //   // Item.appendChild(position);
+  //   // Item.appendChild(postedAt);
+  //   // Item.appendChild(contract);
+  //   // Item.appendChild(location);
+  //   ItemSkill.appendChild(role);
+  //   ItemSkill.appendChild(levelItem);
+
+  //   ItemSkill.appendChild(languages);
+  //   ItemSkill.appendChild(tools);
+  // }
 };
 fetchHandler();
 
@@ -56,11 +117,16 @@ function search() {
   const input = document.querySelector("input");
   let x = document.getElementsByTagName("li");
   for (let j = 0; j < x.length; j++) {
-    if (x[j].childNodes[1].textContent.toLocaleLowerCase().trim.length === 0) {
+    if (
+      x[j].childNodes[0].childNodes[1].textContent.toLocaleLowerCase().trim
+        .length === 0
+    ) {
       x[j].classList.remove("search-none");
     }
     if (
-      !x[j].childNodes[1].textContent.toLocaleLowerCase().includes(input.value)
+      !x[j].childNodes[0].childNodes[1].textContent
+        .toLocaleLowerCase()
+        .includes(input.value)
     ) {
       // x[j].style.display = "none";
       x[j].classList.add("search-none");
@@ -76,7 +142,7 @@ function filter() {
   const text = select.options[select.selectedIndex].text;
   const x = document.getElementsByTagName("li");
   for (let k = 0; k < x.length; k++) {
-    if (x[k].childNodes[7].textContent === text) {
+    if (x[k].childNodes[1].childNodes[0].textContent === text) {
       // x[k].style.display = "list-item";
       x[k].classList.remove("search-none");
       x[k].classList.add("search-display");
