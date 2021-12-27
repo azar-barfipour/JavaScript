99b7e69c2072c5193607e45c84e74ba6c04b780b
Azar Barfipour <azar.barfipour@gmail.com>
2021-12-26 16:50:40 -0800

    function practices

--- Udemy/script-09.js
+++ Udemy/script-09.js
@@ -324,3 +324,81 @@ for(const [key,value] of gameEvents) {
 (key < 45) ?  half = '[FIRST HALF]' : half = '[SECOND HALF]'
 console.log(`${half} ${key} : ${value}`);
 }
+
+//replace and replaceAll
+
+const str = 'please all passengers come to the door and enter from door for boarding.'
+console.log(str.replaceAll('door','gate')); 
+
+
+//splice split , join method
+
+const capitalizeNames = (name) => {
+  const capName= []
+  const names = name.split(' ');
+  console.log(names);
+  for(const n of names){
+    capName.push(n[0].toUpperCase() + n.slice(1));
+  }
+  console.log(capName.join(' ')); 
+}
+
+capitalizeNames('ms. azar barfipour')
+
+// Coding Challenge #4
+
+/* 
+Write a program that receives a list of variable names written in underscore_case and convert them to camelCase.
+The input will come from a textarea inserted into the DOM (see code below), and conversion will happen when the button is pressed.
+THIS TEST DATA (pasted to textarea)
+underscore_case
+ first_name
+Some_Variable 
+  calculate_AGE
+delayed_departure
+SHOULD PRODUCE THIS OUTPUT (5 separate console.log outputs)
+underscoreCase      ✅
+firstName           ✅✅
+someVariable        ✅✅✅
+calculateAge        ✅✅✅✅
+delayedDeparture    ✅✅✅✅✅
+HINT 1: Remember which character defines a new line in the textarea 😉
+HINT 2: The solution only needs to work for a variable made out of 2 words, like a_b
+HINT 3: Start without worrying about the ✅. Tackle that only after you have the variable name conversion working 😉
+HINT 4: This challenge is difficult on purpose, so start watching the solution in case you're stuck. Then pause and continue!
+Afterwards, test with your own test data!
+GOOD LUCK 😀
+*/
+
+document.body.append(document.createElement('textarea'));
+document.body.append(document.createElement('button'));
+document.querySelector('button').addEventListener('click',function(){
+  const text = document.querySelector('textarea').value;
+  console.log(text);
+  const rows = text.split('\n');
+  console.log(rows);
+  for(const [i,row] of rows.entries()){
+    const [first,second] = row.toLowerCase().trim().split('_');
+    const camelCase = `${first}${second.replace(second[0],second[0].toUpperCase())}`;
+    console.log(`${camelCase.padEnd(20)}${'*'.repeat(i+1)}`);
+    // newRow.push(splited[0] + splited[1][0].toUpperCase() + splited[1].slice(1));
+  }
+  // console.log(newRow.join('\n'));
+})
+
+//string practices
+
+const flights =
+  '_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30';
+
+// 🔴 Delayed Departure from FAO to TXL (11h25)
+//              Arrival from BRU to FAO (11h45)
+//   🔴 Delayed Arrival from HEL to FAO (12h05)
+//            Departure from FAO to LIS (12h30)
+
+const newFlight = flights.split('+')
+for(const flight of newFlight){
+  const flightItems = flight.split(';');
+  const [a,b,c,d] = flightItems;
+  console.log(`${a.startsWith('_Delayed') ? '🔴' : ''}${a.replaceAll('_',' ')} from ${b.slice(0,3).toUpperCase()} to ${c.slice(0,3).toUpperCase()} (${d.replace(':','h')})`.padStart(45));
+}
