dd11635362e27fb1743f5679cdfc1e3e3ea471d9
Azar Barfipour <azar.barfipour@gmail.com>
2022-01-12 23:15:23 -0800

    added schrolled in banlist website

--- Bank-website/script.js
+++ Bank-website/script.js
@@ -7,6 +7,8 @@ const modal = document.querySelector(".modal");
 const overlay = document.querySelector(".overlay");
 const btnCloseModal = document.querySelector(".btn--close-modal");
 const btnsOpenModal = document.querySelectorAll(".btn--show-modal");
+const btnScrollTo = document.querySelector(".btn--scroll-to");
+const section1 = document.querySelector("#section--1");
 
 const openModal = function () {
   modal.classList.remove("hidden");
@@ -27,5 +29,33 @@ overlay.addEventListener("click", closeModal);
 document.addEventListener("keydown", function (e) {
   if (e.key === "Escape" && !modal.classList.contains("hidden")) {
     closeModal();
+  }
+});
+
+// smooth schrolling
+btnScrollTo.addEventListener("click", function (e) {
+  e.preventDefault();
+  section1.scrollIntoView({ behavior: "smooth" });
+});
+
+//page navigation
+
+// document.querySelectorAll(".nav__link").forEach((el) => {
+//   el.addEventListener("clich", function (e) {
+//     e.preventDefault();
+//     const id = this.getAtrribute("href");
+//     document.querySelector(id).scrollIntoView({ behavior: "smooth" });
+//   });
+// });
+// 1.add event listener to common parent element
+// 2. determine what element originated the event
+
+document.querySelector(".nav__links").addEventListener("click", function (e) {
+  e.preventDefault();
+  //match
+  if (e.target.classList.contains("nav__link")) {
+    const id = e.target.getAttribute("href");
+    console.log(document.querySelector(id));
+    document.querySelector(id).scrollIntoView({ behavior: "smooth" });
   }
 });
